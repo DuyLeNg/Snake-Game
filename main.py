2855349140aa4b2a2fd05f21ccb7093b44100035
Le Duy Nguyen <duy.le.21.ng@gmail.com>
2025-08-31 19:44:30 -0700

    Add food object and detect collision with snake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,7 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -22,6 +23,10 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
