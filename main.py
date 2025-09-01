b586f8cd56f3354f9f3e32298bfec5715fc2db9d
Le Duy Nguyen <duy.le.21.ng@gmail.com>
2025-08-31 20:08:56 -0700

    Add collision detection for wall and tail

--- main.py
+++ main.py
@@ -30,8 +30,17 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+        snake.extend()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
-screen.exitonclick()
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if segment.distance(snake.head) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
